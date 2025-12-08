7f777ffdb39bc45d7b2fa4f701154f2cd7a624f8
matanaziza@gmail.com <matanaziza@gmail.com>
2025-12-08 15:45:43 +0100

    DAY07 FINITO

--- Day07/22222222222222222222.c
+++ Day07/22222222222222222222.c
@@ -17,55 +17,64 @@
 
 int     main(int ac, char **av)
 {
-    int fd = open(av[ac-1], O_RDONLY);
-    int     i = 0;
-    unsigned long long int **tab;
-    char *str;
-    int     j;
-    tab = malloc(sizeof(unsigned long long int *)*(142));
-    while (i < 142)
-    {
-            j = 0;
-            str = get_next_line(fd);
-            printf("%s", str);
-            tab[i] = malloc(sizeof(unsigned long long int)*(142));
-            while (j < 141)
-            {
-                    tab[i][j] = (unsigned long long int)str[j];
-                    j++;
-            }
-            tab[i][j] = 0;
-            i++;
-            free(str);
-    }
-    printf("\n");
-    i = 1;
-    while (i < 142)
-    {
+        int fd = open(av[ac-1], O_RDONLY);
+        int     i = 0;
+        long long int **tab;
+        char *str;
+        int     j;
+        tab = malloc(sizeof(long long int *)*(142));
+        while (i < 142)
+        {
+                j = 0;
+                str = get_next_line(fd);
+                printf("%s", str);
+                tab[i] = malloc(sizeof(long long int)*(142));
+                while (j < 141)
+                {
+                        tab[i][j] = (long long int)str[j]-'0';
+                        j++;
+                }
+                tab[i][j] = 0;
+                i++;
+                free(str);
+        }
+        printf("\n");
+        i = 1;
+        while (i < 142)
+        {
+                j = 0;
+                while (j < 141)
+                {
+                        if (tab[i-1][j] == -2)
+                                tab[i][j]++;
+                        if (tab[i-1][j] >= 0 && tab[i][j] != -1 && tab[i-1][j] != 0)
+                                tab[i][j] += tab[i-1][j];
+                        if (tab[i][j] == -1 && tab[i-1][j] > tab[i][j])
+                        {
+                                tab[i][j-1]+=(tab[i-1][j]);
+                                tab[i][j+1]+=(tab[i-1][j]);
+                        }
+                        j++;
+                }
+                i++;
+        }
+        i = 141;
+        while (i < 142)
+        {
+                j = 0;
+                while (j < 141)
+                        printf("%lld ", (long long int)tab[i][j++]);
+                i++;
+                printf("\n");
+        }
         j = 0;
+        unsigned long long count = 0;
         while (j < 141)
         {
-            if (tab[i-1][j] == 'S')
-                tab[i][j]++;
-            if (tab[i-1][j] != '-' && tab[i-1][j] != 'S' && tab[i][j] != '-' && tab[i-1][j] != '0')
-                tab[i][j] += (tab[i-1][j]-'0');
-            if (tab[i][j] == '-' && tab[i-1][j] > tab[i][j])
-            {
-                tab[i][j-1]+=(tab[i-1][j]-'0');
-                tab[i][j+1]+=(tab[i-1][j]-'0');
-            }
-            j++;
+                count += tab[141][j];
+				printf("%lld\n", count);
+                j++;
         }
-        i++;
-    }
-    unsigned long long count = 0;
-	j = 0;
-    while (j < 141)
-    {
-        count += ((int)tab[141][j]);
-        j++;
-    }
-    count -= (141*'0');
-    printf("count = %lld\n", count);
-    return (0);
+        printf("count = %lld\n", count);
+        return (0);
 }
